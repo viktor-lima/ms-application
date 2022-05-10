1849c81754d0b1ddbd4e879cec12f5d9db1ed4a0
viktor-lima <joaolimak10@outlook.com>
2022-05-10 01:22:04 -0300

    RestTamplate

---
+++ hr-payroll/src/main/java/com/vkl/hrpayroll/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.vkl.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+}
