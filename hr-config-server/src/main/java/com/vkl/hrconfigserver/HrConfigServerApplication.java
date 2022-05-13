7d0bc6115269bba443b781bed4be84f415e6e09f
viktor-lima <joaolimak10@outlook.com>
2022-05-13 01:40:17 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/vkl/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/vkl/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.vkl.hrconfigserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,10 +8,14 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
-
+public class HrConfigServerApplication implements CommandLineRunner{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {	
 	}
 
 }
