3d28ba8470e2f8ab7e1bd6d27944a721bbf09d44
viktor-lima <joaolimak10@outlook.com>
2022-05-13 15:22:58 -0300

    UserRepository, UserResource and zuul config

--- hr-user/src/main/java/com/vkl/hruser/entities/User.java
+++ hr-user/src/main/java/com/vkl/hruser/entities/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -25,6 +26,7 @@ public class User implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(unique = true)
 	private String email;
 	private String password;
 	
