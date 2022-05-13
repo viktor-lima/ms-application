f867c42807c8c9a504f0945a152995c2ba6dad2d
viktor-lima <joaolimak10@outlook.com>
2022-05-13 14:40:53 -0300

    User, Role, ManyToMany association

--- hr-user/src/main/java/com/vkl/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/vkl/hruser/HrUserApplication.java
@@ -2,7 +2,10 @@ package com.vkl.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
