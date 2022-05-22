79321918a9016bd31530d5f1ba79a400aaeaa1f4
viktor-lima <joaolimak10@outlook.com>
2022-05-22 18:09:38 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/vkl/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/vkl/hroauth/services/UserService.java
@@ -1,13 +1,16 @@
 package com.vkl.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.vkl.hroauth.entities.User;
 import com.vkl.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -16,6 +19,15 @@ public class UserService {
 		User user = userFeignClient.findByEmail(email).getBody();
 		if(user ==null) {
 			throw new IllegalArgumentException("Email not found");			
+		}
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user ==null) {
+			throw new UsernameNotFoundException("Email not found");			
 		}
 		return user;
 	}
